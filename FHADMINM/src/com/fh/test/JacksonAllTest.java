f57c33cacf1be819bdf156ef7eca9d073bd28ff3
johnnyfromly <603050292@qq.com>
2017-05-22 23:00:46 +0800

    git

---
+++ FHADMINM/src/com/fh/test/JacksonAllTest.java
@@ -0,0 +1,19 @@
+package com.fh.test;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+
+
+import com.fh.entity.system.User;
+
+public class JacksonAllTest {
+
+	public static void main(String[] args){
+		ObjectMapper om = new ObjectMapper();
+	
+	}
+}
